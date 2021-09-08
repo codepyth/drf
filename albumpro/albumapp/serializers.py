6b1aafa6afb298beabc260c44b665f6053c2f37f
codepyth <wasimbaloch9@gmail.com>
2021-09-08 16:07:02 +0500

    managed files

--- albumpro/albumapp/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from rest_framework.relations import StringRelatedField
-from .models import *
-
-class AlbumSerializer(serializers.ModelSerializer):
-    albumimages = StringRelatedField(many=True, read_only=True)
-    class Meta:
-        model = Album
-        fields = ['name', 'albumimages']
